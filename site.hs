330469864dea01af82a59b0d873cabb82099b0f6
Herdi <herdisaleh02@gmail.com>
2026-09-13 21:18:52 +0200

    First draft of website
    Added support for posts (under /posts/ folder) and projects (JSON input).
    Also added misc. such as clickable icons for GitHub and LinkedIn, and
    a nice image.

--- site.hs
+++ site.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad (forM_)
+import Data.Aeson
+import Data.Maybe (fromMaybe)
 import Data.Monoid (mappend)
-import Data.Text.Lazy
+import Debug.Trace (traceM, traceShow)
 import Hakyll
 import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
@@ -13,34 +15,128 @@ main :: IO ()
 main =
   do
     hakyllWith defaultConfiguration {destinationDirectory = "docs"} $ do
-      match "style.css" $ do
-        route idRoute
-        compile compressCssCompiler
-
-      create ["index.html"] $ do
-        route idRoute
-        compile $ makeItem (renderMarkup home :: String)
-
       match "style.hs" $ do
         route $ setExtension "css"
         compile $ getResourceString >>= withItemBody (unixFilter "runghc" [])
 
-      match "Home.md" $ do
+      match "images/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+      match "pdfs/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+      create ["index.html"] $ do
+        route idRoute
+        compile $
+          do
+            posts <- recentFirst =<< loadAll "posts/*"
+            postHtmls <- traverse postToBulletHtml posts
+            projectHtmls <- parseProjects
+            makeItem (renderMarkup (homeHTML projectHtmls postHtmls)) >>= relativizeUrls
+
+      match "posts/*" $ do
         route $ setExtension "html"
         compile $
-          pandocCompiler
-            >>= applyTemplate
-              (renderMarkup home)
-              defaultContext
-            >>= relativizeUrls
+          do
+            ident <- getUnderlying
+            title <- getMetadataField' ident "title"
+            date <- getMetadataField' ident "date"
+            body <- (H.preEscapedToHtml . itemBody) <$> pandocCompiler
+            makeItem (renderMarkup (postHTML title date body)) >>= relativizeUrls
+
+data Project = Project {pTitle, pDescription, pUrl :: String, pTags :: [String]} deriving (Show)
+
+instance FromJSON Project where
+  parseJSON = withObject "Project" $ \v ->
+    Project
+      <$> v .: "title"
+      <*> v .: "description"
+      <*> v .: "url"
+      <*> v .: "tags"
+
+parseProjects :: Compiler H.Html
+parseProjects =
+  unsafeCompiler $
+    do
+      projects <- eitherDecodeFileStrict "projects.json"
+      case projects of
+        Left err -> error err
+        Right p ->
+          pure $ projectsGrid p
+
+projectCard :: Project -> H.Html
+projectCard p =
+  H.a ! HA.class_ "project-card" ! HA.href (H.toValue (pUrl p)) $ do
+    H.h3 $ htxt (pTitle p)
+    H.p $ htxt (pDescription p)
+
+projectsGrid :: [Project] -> H.Html
+projectsGrid ps = H.div ! HA.class_ "projects-grid" $ mconcat (map projectCard ps)
+
+postToBulletHtml :: Item String -> Compiler H.Html
+postToBulletHtml post = do
+  let ident = itemIdentifier post
+  mUrl <- getRoute ident
+  title <- getMetadataField' ident "title"
+  date <- getMetadataField' ident "date"
+  let url = maybe "#" toUrl mUrl
+  pure $ H.li $ do
+    H.a ! HA.href (H.toValue url) $ H.toHtml title
+    htxt (" - " <> date)
+
+postHTML :: String -> String -> H.Html -> H.Html
+postHTML title date bodyHtml = H.docTypeHtml $ do
+  H.head $ do
+    H.title (htxt title)
+    stylesheet
+  H.body $ H.main $ do
+    H.h1 (htxt title)
+    H.p $ H.em $ htxt date
+    H.div bodyHtml
 
 -- Home page
-home :: H.Html
-home = H.docTypeHtml $ do
+homeHTML :: H.Html -> [H.Html] -> H.Html
+homeHTML projects posts = H.docTypeHtml $ do
   H.head $ do
-    H.title "$title$"
-    H.link
-      ! (HA.rel "stylesheet")
-      ! (HA.type_ "text/css")
-      ! (HA.href "style.css")
-  H.body $ H.main $ "$body$"
+    H.title "Herdi's Home"
+    stylesheet
+  H.body $
+    H.main $
+      do
+        H.h1 "Herdi Saleh"
+        H.p $ do
+          H.a ! HA.target "_blank" ! HA.href "https://github.com/Herdi2" $
+            H.img ! HA.src "./images/github.svg" ! HA.width "24" ! HA.height "24"
+          H.a ! HA.target "_blank" ! HA.href "https://linkedin.com/in/herdi-saleh" $
+            H.img ! HA.src "./images/linkedin.svg" ! HA.width "24" ! HA.height "24"
+        H.p $ do
+          htxt "Hello, I'm Herdi! This is my site, powered by Haskell (woaw)."
+          htxt "You may find my CV"
+          mkLink "./pdfs/Herdi-Saleh-CV.pdf" "here"
+          htxt "."
+        H.h3 "Projects"
+        projects
+        H.h3 "Posts"
+        H.ul $ mconcat posts
+        H.figure $
+          do
+            H.img ! HA.src "./images/Haskell_House.jpg" ! HA.width "600" ! HA.height "418"
+            H.figcaption $
+              do
+                H.em "Haskell's House"
+                htxt "by Edward Hopper"
+
+stylesheet :: H.Html
+stylesheet =
+  H.link
+    ! (HA.rel "stylesheet")
+    ! (HA.type_ "text/css")
+    ! (HA.href "/style.css")
+
+mkLink :: H.AttributeValue -> String -> H.Html
+mkLink url linkName = H.a ! HA.href url $ (htxt linkName)
+
+htxt :: String -> H.Html
+htxt = H.toHtml
--- style.hs
+++ style.hs
@@ -4,16 +4,25 @@ import Clay
 
 css :: Css
 css =
-  body
-    ? do
-      background (parse "#1A1A1A")
-      color white
-      display flex
-      lineHeight (unitless 1.4)
-      maxWidth (px 600)
-      margin (px 30) auto (px 30) auto
-      fontFamily ["EB Garamond", "Garamond"] [serif]
-      fontSize (px 17)
+  do
+    body
+      ? do
+        background (parse "#1A1A1A")
+        color white
+        display flex
+        lineHeight (unitless 1.4)
+        maxWidth (px 600)
+        margin (px 30) auto (px 30) auto
+        fontFamily ["EB Garamond", "Garamond"] [serif]
+        fontSize (px 17)
+    ".projects-grid" ? do
+      display grid
+      gridGap (px 16)
+    ".project-card" ? do
+      display block
+      textDecoration none
+      border (px 1) solid white
+      padding (px 2) (px 2) (px 2) (px 2)
 
 main :: IO ()
 main = putCss css
