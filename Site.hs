27a1a5de82549328d0c2dbec5d51a5adc3b2263a
Herdi <herdisaleh02@gmail.com>
2026-09-16 18:54:07 +0200

    Full website rewrite
    I wasn't happy with my previous design, so I rewrote everything
    with inspiration from ObsidianMD and the official page of the
    revo language.

--- Site.hs
+++ Site.hs
@@ -2,14 +2,14 @@
 
 import Control.Monad (forM_)
 import Data.Aeson
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromJust, fromMaybe)
 import Data.Monoid (mappend)
 import Debug.Trace (traceM, traceShow)
 import Hakyll
+import Text.Blaze.Html.Renderer.Pretty (renderHtml)
 import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as HA
-import Text.Blaze.Renderer.Pretty
 
 main :: IO ()
 main =
@@ -18,6 +18,10 @@ main =
       match "Style.hs" $ do
         route $ setExtension "css"
         compile $ getResourceString >>= withItemBody (unixFilter "runghc" [])
+
+      match "favicon.svg" $ do
+        route $ setExtension "ico"
+        compile copyFileCompiler
 
       match "images/*" $ do
         route idRoute
@@ -30,11 +34,25 @@ main =
       create ["index.html"] $ do
         route idRoute
         compile $
+          makeItem (renderHtml aboutpage)
+            >>= relativizeUrls
+
+      create ["projects.html"] $ do
+        route idRoute
+        compile $
+          do
+            projects <- parseProjects
+            makeItem (renderHtml $ projectpage projects)
+              >>= relativizeUrls
+
+      create ["posts.html"] $ do
+        route idRoute
+        compile $
           do
             posts <- recentFirst =<< loadAll "posts/*"
-            postHtmls <- traverse postToBulletHtml posts
-            projectHtmls <- parseProjects
-            makeItem (renderMarkup (homeHTML projectHtmls postHtmls)) >>= relativizeUrls
+            postInfo <- traverse postMetadata posts
+            makeItem (renderHtml $ postspage postInfo)
+              >>= relativizeUrls
 
       match "posts/*" $ do
         route $ setExtension "html"
@@ -44,7 +62,8 @@ main =
             title <- getMetadataField' ident "title"
             date <- getMetadataField' ident "date"
             body <- (H.preEscapedToHtml . itemBody) <$> pandocCompiler
-            makeItem (renderMarkup (postHTML title date body)) >>= relativizeUrls
+            makeItem (renderHtml (postpage title date body))
+              >>= relativizeUrls
 
 data Project = Project {pTitle, pDescription, pUrl :: String, pTags :: [String]} deriving (Show)
 
@@ -56,80 +75,25 @@ instance FromJSON Project where
       <*> v .: "url"
       <*> v .: "tags"
 
-parseProjects :: Compiler H.Html
+parseProjects :: Compiler [Project]
 parseProjects =
   unsafeCompiler $
     do
       projects <- eitherDecodeFileStrict "projects.json"
       case projects of
         Left err -> error err
-        Right p ->
-          pure $ projectsGrid p
+        Right p -> pure p
 
-projectCard :: Project -> H.Html
-projectCard project =
-  H.a ! HA.class_ "project-card" ! HA.href (H.toValue (pUrl project)) $ do
-    H.h3 $ htxt (pTitle project)
-    H.p $ htxt (pDescription project)
-    H.div ! HA.class_ "project-tags" $
-      mconcat $
-        ((H.span ! HA.class_ "project-card-tag") . htxt) <$> pTags project
+data Post = Post {poTitle, poDate, poUrl :: String}
 
-projectsGrid :: [Project] -> H.Html
-projectsGrid ps = H.div ! HA.class_ "projects-grid" $ mconcat (map projectCard ps)
-
-postToBulletHtml :: Item String -> Compiler H.Html
-postToBulletHtml post = do
-  let ident = itemIdentifier post
-  mUrl <- getRoute ident
-  title <- getMetadataField' ident "title"
-  date <- getMetadataField' ident "date"
-  let url = maybe "#" toUrl mUrl
-  pure $ H.li $ do
-    H.a ! HA.href (H.toValue url) $ H.toHtml title
-    htxt (" - " <> date)
-
-postHTML :: String -> String -> H.Html -> H.Html
-postHTML title date bodyHtml = H.docTypeHtml $ do
-  H.head $ do
-    H.title (htxt title)
-    stylesheet
-  H.body $ H.main $ do
-    H.h1 (htxt title)
-    H.p $ H.em $ htxt date
-    H.div bodyHtml
-
--- Home page
-homeHTML :: H.Html -> [H.Html] -> H.Html
-homeHTML projects posts = H.docTypeHtml $ do
-  H.head $ do
-    H.title "Herdi's Home"
-    stylesheet
-  H.body $
-    H.main $
-      do
-        H.h1 "Herdi Saleh"
-        H.p $ do
-          H.a ! HA.target "_blank" ! HA.href "https://github.com/Herdi2" $
-            H.img ! HA.src "./images/github.svg" ! HA.width "24" ! HA.height "24"
-          H.a ! HA.target "_blank" ! HA.href "https://linkedin.com/in/herdi-saleh" $
-            H.img ! HA.src "./images/linkedin.svg" ! HA.width "24" ! HA.height "24"
-        H.p $ do
-          htxt "Hello, I'm Herdi! This is my site, powered by Haskell (woaw)."
-          htxt "You may find my CV"
-          mkLink "./pdfs/Herdi-Saleh-CV.pdf" "here"
-          htxt "."
-        H.h3 "Projects"
-        projects
-        H.h3 "Posts"
-        H.ul $ mconcat posts
-        H.figure $
-          do
-            H.img ! HA.src "./images/Haskell_House.jpg" ! HA.width "600" ! HA.height "418"
-            H.figcaption $
-              do
-                H.em "Haskell's House"
-                htxt "by Edward Hopper"
+postMetadata :: Item String -> Compiler Post
+postMetadata post =
+  do
+    let ident = itemIdentifier post
+    url <- getRoute ident
+    title <- getMetadataField' ident "title"
+    date <- getMetadataField' ident "date"
+    return $ Post title date (fromJust url)
 
 stylesheet :: H.Html
 stylesheet =
@@ -138,8 +102,112 @@ stylesheet =
     ! (HA.type_ "text/css")
     ! (HA.href "/Style.css")
 
-mkLink :: H.AttributeValue -> String -> H.Html
-mkLink url linkName = H.a ! HA.href url $ (htxt linkName)
+navbar :: H.Html
+navbar = H.nav ! HA.class_ "sidebar" $
+  do
+    H.h3 $ H.b "Herdi"
+    H.ul $
+      mconcat
+        ( H.li
+            <$> [ mkLink "/" "about",
+                  mkLink "/projects.html" "projects",
+                  mkLink "/posts.html" "posts"
+                ]
+        )
+
+header :: String -> H.Html
+header title =
+  H.head $ do
+    H.title (htxt title)
+    H.link ! HA.rel "icon" ! HA.type_ "image/svg+xml" ! HA.sizes "512x512" ! HA.href "./images/favicon.svg"
+    stylesheet
+
+aboutpage :: H.Html
+aboutpage = H.docTypeHtml $ do
+  header "about"
+  H.body $ do
+    navbar
+    H.article $ do
+      H.h1 "about"
+      H.div ! HA.class_ "button-grid" $ do
+        iconLink "https://github.com/Herdi2" "GitHub" "./images/github.svg" 24 24
+        iconLink "https://linkedin.com/in/herdi-saleh" "LinkedIn" "./images/linkedin.svg" 24 24
+        iconLink "./pdfs/Herdi-Saleh-CV.pdf" "CV" "./images/cv.svg" 24 24
+      H.div $ do
+        H.p $ do
+          htxt "Hello! I'm Herdi. This is my little site, where I show off my projects and (hopefully) write a bit."
+          htxt "I am a former student at "
+          mkLink "https://www.kth.se/" "KTH"
+          htxt " where I recently obtained a Master's degree in Computer Science."
+          htxt "My thesis was in compiler verification using translation validation, applied to the C2 JVM JIT compiler."
+          htxt "Before that I'd interned at Ericsson, working on a fuzzer for their in-house compiler."
+        H.p $ do
+          htxt "Although my interests span most of what CS has to offer, I've fallen into functional programming!"
+          htxt "In fact, "
+          mkLink "https://github.com/Herdi2/my-site" "this site"
+          htxt " is fully generated using Haskell."
+          htxt "I also wrote my thesis tool, "
+          mkLink "https://github.com/Herdi2/C2TranslationValidation" "C2tv"
+          htxt ", in Haskell."
+          htxt "Even Ericsson had me working on a fuzzer written in Haskell!"
+          htxt "It seems the monads are after me..."
+      H.figure $
+        do
+          H.img ! HA.src "./images/Haskell_House.jpg" ! HA.width "600" ! HA.height "418"
+          H.figcaption $
+            do
+              H.em "Haskell's House"
+              htxt "by Edward Hopper"
+
+projectpage :: [Project] -> H.Html
+projectpage projects = H.docTypeHtml $ do
+  header "Projects"
+  H.body $ do
+    navbar
+    H.article $ H.div ! HA.class_ "projects-grid" $ mconcat (map projectitem projects)
+
+projectitem :: Project -> H.Html
+projectitem project = do
+  H.div $ do
+    H.h3 $ mkLink (pUrl project) (pTitle project)
+    H.p $ htxt (pDescription project)
+    H.div $
+      mconcat $
+        ((H.span ! HA.class_ "project-tag") . htxt) <$> pTags project
+
+postspage :: [Post] -> H.Html
+postspage posts = H.docTypeHtml $ do
+  header "Posts"
+  H.body $ do
+    navbar
+    H.article $ H.div ! HA.class_ "projects-grid" $ mconcat (map postitem posts)
+
+postitem :: Post -> H.Html
+postitem post = do
+  H.div $ do
+    H.em $ mkLink (poUrl post) (poTitle post)
+    htxt "-"
+    htxt (poDate post)
+
+postpage :: String -> String -> H.Html -> H.Html
+postpage title date bodyHtml = H.docTypeHtml $ do
+  header title
+  H.body $ H.main $ do
+    navbar
+    H.article $ do
+      H.h1 (htxt title)
+      H.p $ H.em $ htxt date
+      H.div bodyHtml
+
+iconLink :: String -> String -> String -> Integer -> Integer -> H.Html
+iconLink url linkname imgsrc w h =
+  H.div $
+    do
+      H.img ! HA.src (H.toValue imgsrc) ! HA.width (H.toValue w) ! HA.height (H.toValue h)
+      mkLink url linkname
+
+mkLink :: String -> String -> H.Html
+mkLink url linkName = H.a ! HA.href (H.toValue url) $ (htxt linkName)
 
 htxt :: String -> H.Html
 htxt = H.toHtml
--- Style.hs
+++ Style.hs
@@ -1,42 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Clay
+import Prelude hiding (rem)
+
+backgroundcolor :: Color
+backgroundcolor = "#1A1A1A"
+
+navbarcolor :: Color
+navbarcolor = "#101010"
+
+linkcolor :: Color
+linkcolor = "#8d84bb"
+
+navbarSize :: Size LengthUnit
+navbarSize = vw 10
 
 css :: Css
 css =
   do
     body
       ? do
-        background (parse "#1A1A1A")
+        background backgroundcolor
         color white
-        display flex
-        lineHeight (unitless 1.4)
-        maxWidth (px 600)
-        margin (px 30) auto (px 30) auto
         fontFamily ["EB Garamond", "Garamond"] [serif]
         fontSize (px 17)
+    "a" ? do
+      color linkcolor
+    "a" # hover ? do
+      color backgroundcolor
+      backgroundColor linkcolor
+
+    ".sidebar" ? do
+      width navbarSize
+      background navbarcolor
+      position absolute
+      top (px 0)
+      bottom (px 0)
+      "ul" ? do
+        paddingLeft (vw 0)
+        listStyle none outside none
+
+    "article" ? do
+      marginLeft navbarSize
+      padding (rem 2) (rem 4) (rem 4) (rem 4)
+      background backgroundcolor
+
     ".projects-grid" ? do
       display grid
-      gridGap (px 16)
-    ".project-card" ? do
-      display block
-      textDecoration none
-      color inherit
-      border (px 1) solid white
-      borderRadius (px 30) (px 30) (px 30) (px 30)
-      paddingTop (px 2)
-      paddingBottom (px 2)
-      paddingLeft (px 8)
-      paddingRight (px 8)
-      transition "all" (sec 0.1) ease (sec 0)
-    ".project-card" # hover ? do
-      background (parse "#2A2A2A")
-      borderColor (parse "#888888")
-      transform (translateY (px (-2)))
-    ".project-tags" ? do
-      paddingBottom (px 8)
-      paddingLeft (px 8)
-    ".project-card-tag" ? do
+      gridGap (rem 1)
+
+    ".project-tag" ? do
       display inlineBlock
       color inherit
       borderRadius (px 30) (px 30) (px 30) (px 30)
@@ -44,6 +57,12 @@ css =
       border (px 1) solid (parse "#888888")
       fontSize (px 13)
       whiteSpace nowrap
+
+    ".button-grid" ? do
+      display grid
+      gridGap (rem 1)
+      width maxContent
+      gridTemplateColumns [fr 1, fr 1, fr 1]
 
 main :: IO ()
 main = putCss css
